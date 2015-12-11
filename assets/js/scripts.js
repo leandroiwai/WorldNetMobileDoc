fe9721b3987a998f7ccbb13825e7f98fe96094ab
leandroiwai <leandroiwai@gmail.com>
2015-12-11 14:08:05 +0000

    Initial Commit of WorldNet Doc Files

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('#nav').affix({
+		offset: {
+			top: $('#nav').offset().top,
+			bottom: ($('footer').outerHeight(true) + $('.application').outerHeight(true)) + 40
+		}
+	});
+});
