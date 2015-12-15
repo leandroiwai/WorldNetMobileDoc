e635c9c7988f4d55cec0df6e434de37e70957864
Rafael Santiago <rmxsantiago@gmail.com>
2015-12-15 10:23:04 +0000

    Added new content.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
 $('#nav').affix({
     offset: {     
-      top: $('#nav').offset().top,
-      bottom: ($('footer').outerHeight(true) + $('.application').outerHeight(true)) + 40
+        top: $('#nav').offset().top,
+        bottom: ($('footer').outerHeight(true) + $('.application').outerHeight(true)) + 40
     }
 });
+
+/** Do some stuff when the page is completely loaded **/
+$( document ).ready(function() {
+    loadContent();
+});
+
+/** It loads all the contents one-by-one in the index.html **/
+function loadContent(){
+    loadPage("content/introduction.html", "#introduction");
+    loadPage("content/getting-started.html", "#getting-started");
+    loadPage("content/transactions.html", "#transactions");
+}
+
+/**
+    It loads an html page inside a div targeted by teh div id, and re-aply the code highlight,
+    just in case there are some code snipper in the loaded page.
+**/
+function loadPage(page, target){
+    $(target).load(page, function(){
+        $('pre code').each(function(i, block) {
+            hljs.highlightBlock(block);
+        });
+    });
+}
