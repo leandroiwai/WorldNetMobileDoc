8cdb1bffe2f495a290b66356475568cb2036d31a
Rafael Santiago <rmxsantiago@gmail.com>
2016-01-11 09:55:11 +0000

    The basic behaviour was finished.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,47 @@ $('#nav').affix({
     }
 });
 
+
 /** Do some stuff when the page is completely loaded **/
-$( document ).ready(function() {
-    loadContent();
+$(document).ready(function() {
+    var platform = getQueryVariable("platform");
+    var version = getQueryVariable("version");
+    
+    if(platform === false)
+        platform = "ios";
+    
+    if(version === false){
+        version = "4.0.0";
+    }
+    
+    $("#dropdown-version-title").html(version); 
+    
+    $('[data-spy="scroll"]').each(function () {
+        var $spy = $(this).scrollspy('refresh');
+    });
+    
+    loadContent(platform, version);
 });
 
 /** It loads all the contents one-by-one in the index.html **/
-function loadContent(){
-    loadPage("content/introduction.html", "#introduction");
-    loadPage("content/getting-started.html", "#getting-started");
-    loadPage("content/transactions.html", "#transactions");
+function loadContent(platform, version){    
+    loadPage("content/" + version + "/floating-menu.html", "#nav");
+    
+    loadPage("content/" + version + "/" + platform + "/introduction.html", "#introduction");    
+    loadPage("content/" + version + "/" + platform + "/getting-started.html", "#getting-started");
+    loadPage("content/" + version + "/" + platform + "/transactions.html", "#transactions");
+    
+    loadPage("content/" + version + "/" + platform + "/download-page.html", "#download");
+    
+    //$('input[value=android]').closest('.btn').button('toggle');    
+    
+    var url =  returnURL() + "?platform=" + platform + "&version=" + version;
+    console.log(url);
+    history.pushState("page", "caption", url);
+}
+
+function returnURL(){     
+    return $(location).attr('href').replace(/\?*platform=([A-Za-z]+)&version=([0-9\.]+)/g,"");
 }
 
 /**
@@ -23,8 +54,69 @@ function loadContent(){
 **/
 function loadPage(page, target){
     $(target).load(page, function(){
-        $('pre code').each(function(i, block) {
+        $('pre code').each(function(i, block) { 
             hljs.highlightBlock(block);
         });
+    });
+}
+
+/** Get a value from a URL **/
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+}
+
+/** Update the dropdown version title **/
+$(".dropdown-menu").on('click', 'li a', function () {
+    $('#dropdown-version-title').html($(this).html());
+    loadContent($('input[name=options]:checked').val(), $("#dropdown-version-title").text());
+});
+
+$("#radio-group input").on('change', function () {
+    loadContent($('input[name=options]:checked').val(), $("#dropdown-version-title").text());
+});
+
+
+
+/** Zip file **/
+function deferredAddZip(url, filename, zip) {    
+    var deferred = $.Deferred();
+    JSZipUtils.getBinaryContent(url, function (err, data) {
+        if(err) {
+            deferred.reject(err);
+        } else {
+            zip.file(filename, data, {binary:true});
+            deferred.resolve(data);
+        }
+    });
+    return deferred;
+}
+
+
+
+function zip(){
+    var zip = new JSZip();
+    
+    var deferred = $.Deferred(),
+        deferreds = [];
+    
+    $("#download_form").find(":checked").each(function () {
+        var $this = $(this);
+        var url = $this.data("url");
+        var filename = url.replace(/.*\//g, "");
+        deferreds.push(deferredAddZip(url, filename, zip));
+    });
+
+    $.when.apply($, deferreds).done(function () {
+
+        var content = zip.generate({type:"blob"});
+        // see FileSaver.js
+        saveAs(content, "example.zip");
     });
 }
